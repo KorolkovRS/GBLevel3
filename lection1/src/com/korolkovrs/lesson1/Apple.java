4115643765ff66922ded1f5b850162d7d27c1236
KorolkovRS <KorolkovRS@yandex.ru>
2020-08-20 23:39:47 +0300

    Add homework lesson-1

---
+++ lection1/src/com/korolkovrs/lesson1/Apple.java
@@ -0,0 +1,7 @@
+package com.korolkovrs.lesson1;
+
+public class Apple extends Fruit {
+    public Apple() {
+        setWight(1.1f);
+    }
+}
---
+++ lection1/src/com/korolkovrs/lesson1/Fruit.java
@@ -0,0 +1,13 @@
+package com.korolkovrs.lesson1;
+
+abstract public class Fruit {
+    private float wight;
+
+    public float getWight() {
+        return wight;
+    }
+
+    public void setWight(float wight) {
+        this.wight = wight;
+    }
+}
