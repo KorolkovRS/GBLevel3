2262c29af1a9c60202170c749630335957e1b93f
KorolkovRS <KorolkovRS@yandex.ru>
2020-09-09 00:03:06 +0300

    @Test replace with @ParameterizedTest

--- lesson6/src/test/java/ru/korolkovrs/lesson6/MyArrayTest.java
+++ lesson6/src/test/java/ru/korolkovrs/lesson6/MyArrayTest.java
@@ -3,22 +3,29 @@ package ru.korolkovrs.lesson6;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.function.Executable;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
 
 public class MyArrayTest {
 
-    @Test
-    public void checkOutputArray1(){
-        Assertions.assertArrayEquals(MyArray.elementsAfterLastFour(new int[] {1,3,2,4,5,62,2}), new int[] {5,62,2});
+    @ParameterizedTest
+    @MethodSource("arrayStream")
+    public void checkOutputArray(int[] array, int[] result) {
+        Assertions.assertArrayEquals(MyArray.elementsAfterLastFour(array), result);
     }
 
-    @Test
-    public void checkOutputArray2(){
-        Assertions.assertArrayEquals(MyArray.elementsAfterLastFour(new int[] {1,3,2,4,5,62,4}), new int[] {});
-    }
-
-    @Test
-    public void checkOutputArray3(){
-        Assertions.assertArrayEquals(MyArray.elementsAfterLastFour(new int[] {4,1,3,2,5,62}), new int[] {1,3,2,5,62});
+    static Stream<Arguments> arrayStream() {
+        List<Arguments> arguments = Arrays.asList(
+                Arguments.of(new int[] {1,3,2,4,5,62,2}, new int[] {5,62,2}),
+                Arguments.of(new int[] {1,3,2,4,5,62,4}, new int[] {}),
+                Arguments.of(new int[] {4,1,3,2,5,62}, new int[] {1,3,2,5,62})
+                );
+               return arguments.stream();
     }
 
     @Test
@@ -31,23 +38,20 @@ public class MyArrayTest {
         });
     }
 
-    @Test
-    public void checkContainsOneOrFourInArray1() {
-        Assertions.assertEquals(MyArray.containOneOrFour(new int[] {1,3,2,5,62}), true);
+    @ParameterizedTest
+    @MethodSource("arrayAndStatementSteam")
+
+    public void checkContainsOneOrFourInArray(int[] array, boolean statement) {
+        Assertions.assertEquals(MyArray.containOneOrFour(array), statement);
     }
 
-    @Test
-    public void checkContainsOneOrFourInArray2() {
-        Assertions.assertEquals(MyArray.containOneOrFour(new int[] {3,2,5,62,4}), true);
-    }
-
-    @Test
-    public void checkContainsOneOrFourInArray3() {
-        Assertions.assertEquals(MyArray.containOneOrFour(new int[] {}), false);
-    }
-
-    @Test
-    public void checkContainsOneOrFourInArray4() {
-        Assertions.assertEquals(MyArray.containOneOrFour(new int[] {6,3,2,5,62}), false);
+    static Stream<Arguments> arrayAndStatementSteam() {
+        List<Arguments> arguments = Arrays.asList(
+                Arguments.of(new int[] {1,3,2,5,62}, true),
+                Arguments.of(new int[] {3,2,5,62,4}, true),
+                Arguments.of(new int[] {}, false),
+                Arguments.of(new int[] {6,3,2,5,62}, false)
+        );
+        return arguments.stream();
     }
 }
